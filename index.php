028224e91d0e3f944bee19762053157bf6cf5565
robbishimozuki <robbishimozuki@gmail.com>
2020-01-13 08:33:27 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>ICHIRAKU</title>
+<link rel="icon" href="images/favicon5.ico" style="width: 32px; height: 32px">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Cookery Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!---->
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,100,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+<!-- start-smoth-scrolling -->
+		<script type="text/javascript" src="js/move-top.js"></script>
+		<script type="text/javascript" src="js/easing.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+		</script>
+	<!-- start-smoth-scrolling -->
+<link href="css/styles.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<!-- animation-effect -->
+<link href="css/animate.min.css" rel="stylesheet"> 
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+<!-- //animation-effect -->
+
+</head>
+<body>
+<script src='../../../../../ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script src="../../../../../m.servedby-buysellads.com/monetization.js" type="text/javascript"></script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+  	}
+})();
+</script>
+<script>
+(function(){
+if(typeof _bsa !== 'undefined' && _bsa) {
+	// format, zoneKey, segment:value, options
+	_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+}
+})();
+</script>
+<script>
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+  	}
+})();
+</script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','../../../../../www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-30027142-1', 'w3layouts.com');
+  ga('send', 'pageview');
+</script>
+<body>
+ <div class="header">
+	<div class="container">
+		<div class="logo animated wow pulse" data-wow-duration="1000ms" data-wow-delay="500ms">
+			<h1 style="float: left"><a href="index.php"><span>Ichi</span><img src="images/oo.png" alt="">raku</a>
+		</div>
+		<?php require("header_menu.php") ?>
+	<div class="clearfix"></div>
+	</div>
+	<!-- start search-->	
+		<div class="banner">
+			<p class="animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">WE ARE ALL ABOUT FOOD</p>
+			<label></label>
+			<h4 class="animated wow fadeInTop" data-wow-duration="1000ms" data-wow-delay="500ms">こんにちは</h4>
+			<a class="scroll down" href="#content-down"><img src="images/down.png" alt=""></a>
+		</div>
+</div>
+<!--content-->
+<!---728x90--->
+<script src='../../../../../publisher.eboundservices.com/dynamicAds/dynamicScript.js'></script>
+<div style='margin: 0 auto;text-align: center;margin-top: 5px;'><script>
+var allowedNumberOfEboundDynamicAdds = 4;
+var sizesEboundDynamicAdsDesktop = ['728x90'];
+var sizesEboundDynamicAdsTablet = ['728x90'];
+var sizesEboundDynamicAdsMobile = ['320x100'];
+eboundAdsTagByDevice(sizesEboundDynamicAdsDesktop,sizesEboundDynamicAdsTablet,sizesEboundDynamicAdsMobile, 'ebound_header_tag');
+
+if(typeof user_tag_config == 'undefined'){
+	var user_tag_config = {};
+}
+user_tag_config['ebound_header_tag'] = {};
+user_tag_config['ebound_header_tag']['desktop'] = {};
+user_tag_config['ebound_header_tag']['desktop']['cpm'] = '';
+user_tag_config['ebound_header_tag']['desktop']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['tablet'] = {};
+user_tag_config['ebound_header_tag']['tablet']['cpm'] = '';
+user_tag_config['ebound_header_tag']['tablet']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['mobile'] = {};
+user_tag_config['ebound_header_tag']['mobile']['cpm'] = '';
+user_tag_config['ebound_header_tag']['mobile']['adsCode'] = '';
+</script></div>
+<div class="content" id="content-down">
+	<div class="content-top-top">
+		<div class="container">
+			<div class="content-top">
+				<div class="col-md-4 content-left animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<h3>About</h3>
+					<label><i class="glyphicon glyphicon-menu-up"></i></label>
+					<span>What and how we do?</span>
+				</div>
+				<div class="col-md-8 content-right animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<p>ichiraku Restaurantは、インドの西ベンガルに本拠を置く食品注文および配送会社です。インド料理店は、最高の近所のレストランから都会の食通まで、完全な食品注文および配達ソリューションを提供するという考えに触発されました。幅広いレストランから注文するための単一のウィンドウである当社には、レストランからの注文をピックアップして顧客に配達するための、専任の配送スタッフがいます。独自のフリートを持つことで、どのレストランでも最低注文ポリシーをお客様に提供し、提携しているすべてのパートナーレストランのオンライン支払いを受け入れる柔軟性が得られます。配送担当者は一度に1つの注文を運ぶため、信頼性が高く迅速な配送が可能になります。</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="content-mid">
+					
+				<div class="col-md-4 food-grid animated wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<div class=" hover-fold">
+					  <h4>No minimum order</h4>
+					  <div class="top">
+						<div class="front face"></div>
+						<div class="back face">
+						<p></p>
+						</div>
+					  </div>
+					  <div class="bottom"></div>
+					</div>
+				</div>
+				<div class="col-md-4 food-grid animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<div class=" hover-fold">
+					  <h4>Delicious & Healthy</h4>
+					  <div class="top">
+						<div class="front face front1"></div>
+						<div class="back face">
+						<p></p>
+						</div>
+					  </div>
+					  <div class="bottom bottom1"></div>
+					</div>
+				</div>
+				<div class="col-md-4 food-grid animated wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<div class=" hover-fold">
+					  <h4>Affordable prices</h4>
+					  <div class="top">
+						<div class="front face front2"></div>
+						<div class="back face">
+						<p></p>
+						</div>
+					  </div>
+					  <div class="bottom bottom2"></div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--services-->
+<!---728x90--->
+<script src='../../../../../publisher.eboundservices.com/dynamicAds/dynamicScript.js'></script>
+<div style='margin: 0 auto;text-align: center;margin-top: 5px;'><script>
+var allowedNumberOfEboundDynamicAdds = 4;
+var sizesEboundDynamicAdsDesktop = ['728x90'];
+var sizesEboundDynamicAdsTablet = ['728x90'];
+var sizesEboundDynamicAdsMobile = ['320x100'];
+eboundAdsTagByDevice(sizesEboundDynamicAdsDesktop,sizesEboundDynamicAdsTablet,sizesEboundDynamicAdsMobile, 'ebound_header_tag');
+
+if(typeof user_tag_config == 'undefined'){
+	var user_tag_config = {};
+}
+user_tag_config['ebound_header_tag'] = {};
+user_tag_config['ebound_header_tag']['desktop'] = {};
+user_tag_config['ebound_header_tag']['desktop']['cpm'] = '';
+user_tag_config['ebound_header_tag']['desktop']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['tablet'] = {};
+user_tag_config['ebound_header_tag']['tablet']['cpm'] = '';
+user_tag_config['ebound_header_tag']['tablet']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['mobile'] = {};
+user_tag_config['ebound_header_tag']['mobile']['cpm'] = '';
+user_tag_config['ebound_header_tag']['mobile']['adsCode'] = '';
+</script></div>
+<div class="services">
+	<div class="container">
+		<div class="services-top">		
+			<div class="col-md-8 services-right animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					
+			</div>
+			<div class="col-md-4 services-left animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+				<h3>Services</h3>
+				<label><i class="glyphicon glyphicon-menu-up"></i></label>
+				<span>Our mission is to ensure nobody has a bad meal.</span>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="service-top">
+			<div class="col-md-5 service-top animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+				<div class=" service-grid">
+					<div class="col-md-6 service-grid1">
+						<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+							<a href="#" class="hi-icon hi-icon-mobile glyphicon glyphicon-leaf"></a>
+						</div>
+					</div>
+					<div class="col-md-6 service-grid1">
+						<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+							<a href="#" class="hi-icon hi-icon-mobile glyphicon glyphicon-star-empty"></a>
+						</div>					
+						</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class=" service-grid">
+					<div class="col-md-6 service-grid1">
+						<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+							<a href="menu.php" class="hi-icon hi-icon-mobile glyphicon glyphicon-folder-open"></a>
+						</div>					
+						</div>
+					<div class="col-md-6 service-grid1">
+						<div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
+							<a href="#" class="hi-icon hi-icon-mobile glyphicon glyphicon-home"></a>
+						</div>
+						</div>
+					<div class="clearfix"> </div>
+				</div>
+				
+			</div>
+			<div class="col-md-7 service-bottom animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+				<div class=" service-bottom1">
+					<div class=" ser-bottom">
+						<img src="images/sa.jpg" class="img-responsive" alt="">
+					</div>
+					<div class="ser-top "> 
+						<h5>Helping people discover great places around them</h5>
+						<p>Our team gathers information from every restaurant on a regular basis to ensure our data is fresh. Our vast community of food lovers share
+						their reviews and photos, so you have all that you need to make an informed choice.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class=" service-bottom1">
+					<div class=" ser-bottom">
+						<img src="images/sa1.jpg" class="img-responsive" alt="">
+					</div>
+					<div class="ser-top "> 
+						<h5>Building amazing experiences around dining</h5>
+						<p>Starting with information for over 1 million restaurants (and counting) globally, we're making dining smoother and more enjoyable with services
+						like online ordering and table reservations.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<div class=" service-bottom1">
+					<div class=" ser-bottom">
+						<img src="images/sa2.jpeg" class="img-responsive" alt="">
+					</div>
+					<div class="ser-top "> 
+						<h5>Enabling restaurants to create amazing experiences</h5>
+						<p>With dedicated engagement and management tools, we're enabling restaurants to spend more time focusing on food itself, which translates directly 
+						to better dining experiences.</p>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!--//services-->
+<!---728x90--->
+<script src='../../../../../publisher.eboundservices.com/dynamicAds/dynamicScript.js'></script>
+<div style='margin: 0 auto;text-align: center;margin-top: 5px;'><script>
+var allowedNumberOfEboundDynamicAdds = 4;
+var sizesEboundDynamicAdsDesktop = ['728x90'];
+var sizesEboundDynamicAdsTablet = ['728x90'];
+var sizesEboundDynamicAdsMobile = ['320x100'];
+eboundAdsTagByDevice(sizesEboundDynamicAdsDesktop,sizesEboundDynamicAdsTablet,sizesEboundDynamicAdsMobile, 'ebound_header_tag');
+
+if(typeof user_tag_config == 'undefined'){
+	var user_tag_config = {};
+}
+user_tag_config['ebound_header_tag'] = {};
+user_tag_config['ebound_header_tag']['desktop'] = {};
+user_tag_config['ebound_header_tag']['desktop']['cpm'] = '';
+user_tag_config['ebound_header_tag']['desktop']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['tablet'] = {};
+user_tag_config['ebound_header_tag']['tablet']['cpm'] = '';
+user_tag_config['ebound_header_tag']['tablet']['adsCode'] = '';
+user_tag_config['ebound_header_tag']['mobile'] = {};
+user_tag_config['ebound_header_tag']['mobile']['cpm'] = '';
+user_tag_config['ebound_header_tag']['mobile']['adsCode'] = '';
+</script></div>
+<!--news-->
+<!--footer-->
+<!--
+	<div class="footer">
+		<div class="container">
+			<div class="footer-head">
+				<div class="col-md-8 footer-top animated wow fadeInRight" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<ul class=" in">
+						<li><a href="index-2.html">Home</a></li>
+						<li><a  href="menu.html">Menu</a></li>
+						<li><a  href="blog.html">Blog</a></li>
+						<li><a  href="events.html">Events</a></li>
+						<li><a  href="contact.html">Contact</a></li>
+					</ul>					
+				</div>
+				<div class="col-md-4 footer-bottom  animated wow fadeInLeft" data-wow-duration="1000ms" data-wow-delay="500ms">
+					<h2>Follow Indian Restaurant</h2>
+					<label><i class="glyphicon glyphicon-menu-up"></i></label>
+					<p></p>
+					<ul class="social-ic">
+						<li><a href="#"><i></i></a></li>
+						<li><a href="#"><i class="ic"></i></a></li>
+						<li><a href="#"><i class="ic1"></i></a></li>
+						<li><a href="#"><i class="ic2"></i></a></li>
+						<li><a href="#"><i class="ic3"></i></a></li>
+					</ul>
+
+				</div>
+			<div class="clearfix"> </div>
+					
+			</div>
+			<p class="footer-class animated wow bounce" data-wow-duration="1000ms" data-wow-delay="500ms">&copy; 2016 Cookery . All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank">W3layouts</a> </p>
+		</div>
+	</div>		
+-->
+	<!--//footer-->
+</body>
+<?php require("footer.php") ?>		
+<!-- Mirrored from p.w3layouts.com/demos/21-03-2016/cookery/web/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 27 Aug 2018 13:20:11 GMT -->
+</html>
